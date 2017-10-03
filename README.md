2027363655135574f0cd246bfb591504dfecb074
dalanao <dal437@nyu.edu>
2017-10-02 23:16:35 -0400

    Attempting to fix issues for Part One

---
+++ README.md
@@ -0,0 +1,4 @@
+Name: Diego Lanao
+NetID: dal437
+Project: Homework #03 - Tiny Web Framework
+
--- src/warmUp.js
+++ src/warmUp.js
@@ -1,1 +1,22 @@
 // warmUp.js
+
+const net = require('net');
+const PORT = 8080;
+const HOST = '127.0.0.1';
+
+
+const server = net.createServer((sock) => {
+  console.log('client connected:', sock.remoteAddress, sock.remotePort);
+  //sock.on('data', applyingData.bind(null, sock));
+  sock.on('data', (receivedData) => { applyingData(sock, receivedData); });
+});
+
+
+function applyingData(sock, receivedData) {
+  console.log("server is handling call");
+  sock.write(`HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<em>hello</em> <strong>world</strong>`);
+  sock.end();
+}
+
+server.listen(PORT, HOST);
+console.log("server is listening");
