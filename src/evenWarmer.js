4af31cb9fce28e842ec5583319ebca8140b8c714
dalanao <dal437@nyu.edu>
2017-10-04 17:38:29 -0400

    Completed Request Class for Part Two

--- src/evenWarmer.js
+++ src/evenWarmer.js
@@ -5,24 +5,43 @@ const net = require('net');
 
 class Request {
   constructor(httpRequest){
+    this.httpRequest = httpRequest;
     const news = httpRequest.split('\r\n');
     this.method = news[0].split(" ")[0];
     this.path = news[0].split(" ")[1];
 
-    const x = news[1].split(" ")[0];
-    const y = news[1].split(" ")[1];
-    const a = news[2].split(" ")[0];
-    const b = news[2].split(" ")[1];
+    const headers = {};
+    const hostIndex = 1;
 
+    let refererIndex = 2;
+    if (news[2] === '') {
+       refererIndex = -1;
+    }
+
+    let bodyIndex = 4;
+    if (refererIndex === -1) {
+       bodyIndex = 3;
+    }
+
+    const x = news[hostIndex].split(" ")[0];
     const newx = x.substring(0, x.length - 1);
-    const newa = a.substring(0, a.length - 1);
+    const y = news[hostIndex].split(" ")[1];
+    headers[newx] = y;
 
-    this.headers = {[newx]:y, [newa]:b};
+    if (refererIndex > 0) {
+      const a = news[refererIndex].split(' ')[0];
+      const newa = a.substring(0, a.length - 1);
+      const b = news[refererIndex].split(' ')[1];
 
+      headers[newa] = b;
+    }
+    this.headers = headers;
+    this.body = news[bodyIndex];
+  }
 
-    console.log(this.method);
-    console.log(this.path);
-    console.log(this.headers);
+  toString(){
+    let httpRequest = this.httpRequest;
+    return httpRequest;
   }
 }
 
@@ -33,13 +52,11 @@ const HOST = '127.0.0.1';
 
 const server = net.createServer((sock) => {
   console.log('client connected:', sock.remoteAddress, sock.remotePort);
-  //sock.on('data', applyingData.bind(null, sock));
   sock.on('data', (receivedData) => { applyingData(sock, receivedData); });
 });
 
 
 function applyingData(sock, receivedData) {
-  const req = new Request(s);
   console.log("server is handling call");
   sock.write(`HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<em>hello</em> <strong>world</strong>`);
   sock.end();
